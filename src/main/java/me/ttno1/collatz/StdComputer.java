666ef4ecc174203a740bff54cc6684b6cfeb3638
TTNO1 <gruwe789@gmail.com>
2021-08-21 07:38:24 -0400

    Added "break;" to StdComputer
     On branch ComputeOnlyOnce
     Your branch is up to date with 'github/ComputeOnlyOnce'.
    
     Changes to be committed:
    	modified:   src/main/java/me/ttno1/collatz/StdComputer.java

--- src/main/java/me/ttno1/collatz/StdComputer.java
+++ src/main/java/me/ttno1/collatz/StdComputer.java
@@ -101,6 +101,8 @@ public class StdComputer implements Computer {
         		
         		statsHolder.setReference(input, computedCoord);
         		
+        		break;
+        		
         	}
             
             statsHolder.addStepToPath(input, num);
